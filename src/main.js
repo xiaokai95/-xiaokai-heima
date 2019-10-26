9336c1dc36be767af88e3d7b3835a69ecbc8017c
xiaokai95 <2857318178@qq.com>
2019-10-26 21:25:44 +0800

    登录页面验证完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import ElementUi from 'element-ui'
 import router from '@/router'
 import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import axios from '@/api'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUi)
-
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,21 +2,22 @@
   <div class="cantainer">
     <el-card>
       <img src="../../assets/logo_index.png" alt />
-      <el-form :model="loginForm">
-        <el-form-item>
-          <el-input v-model="loginForm.name" placeholder="请输入手机号" maxlength="11"></el-input>
+      <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号" maxlength="11"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input
             v-model="loginForm.code"
             placeholder="请输入验证码"
+            maxlength="6"
             style="width:280px;margin-right:18px"
           ></el-input>
           <el-button>获取验证码</el-button>
-          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
+        <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button type="primary" style="width:100%;margin-top:20px" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -26,11 +27,47 @@
 <script>
 export default {
   data () {
+    const validatorName = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号码格式不正确'))
+      }
+    }
     return {
       loginForm: {
-        name: '',
+        mobile: '',
         code: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          {
+            validator: validatorName,
+            trigger: 'blur'
+          }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码是6位', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs['loginForm'].validate(valid => {
+        if (valid) {
+          this.$http
+            .post('authorizations', this.loginForm)
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
     }
   }
 }
